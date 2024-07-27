951b4239ff293d0e1ac96d50179b010b837796bc
iurylemos <iurylemos4@gmail.com>
2024-07-27 13:47:17 -0300

    feat(): addin tailwind for application

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { prisma } from "./database";
+import { Liquid } from "liquidjs";
 
 dotenv.config();
 
 const application = express();
 const PORT = process.env.PORT || 3000;
 
+const liquidEngine = new Liquid();
+
+application.use(express.static("public"));
+
+application.engine("liquid", liquidEngine.express());
+application.set("views", "./src/views");
+application.set("view engine", "liquid");
+
 application.get("/", (req: Request, resp: Response) => {
-  resp.send("Express + Typescript Server");
+  resp.render("index");
 });
 
-application.get("/db", (req: Request, resp: Response) => {
-  resp.send("Express + Typescript Server");
+application.get("/db", async (req: Request, resp: Response) => {
+  try {
+    const total = await prisma.todo.count();
+    resp.send({ total });
+  } catch (error) {
+    console.log("error");
+  }
 });
 
 application.listen(PORT, () => {
